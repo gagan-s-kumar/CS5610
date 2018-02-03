b77c1e8ed397b7b6451751f2719eb4ab4723cf4d
gagan-s-kumar <gaganskumar9@gmail.com>
2018-02-03 18:50:19 -0500

    First Commit to HW4

---
+++ calc/lib/calc.ex
@@ -0,0 +1,51 @@
+defmodule Calc do
+  @moduledoc """
+  Documentation for Calc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Calc.hello
+      :world
+
+  """
+  def main() do
+    case IO.gets(">") do
+      :eof ->
+        IO.puts "All done"
+      {:error, reason} ->
+        IO.puts "Error: #{reason}"
+      line ->
+        eval(line)
+        main()
+    end
+  end
+
+  def eval(line) do
+    list = String.split(String.trim(line))
+    IO.inspect(list)
+    list
+    |> Enum.map(&push_to_stack/1)
+  end
+
+  def push_to_stack(str) do
+    push_to_stack(str, [], [])
+  end
+
+  #acc1 for integer, acc2 for operands
+
+  def push_to_stack(str, acc1, acc2) when str == "+" do
+    acc2 = acc2 ++ [str]
+    IO.inspect(str)
+    IO.inspect(acc1)
+    IO.inspect(acc2)
+  end
+  
+  def push_to_stack(str, acc1, acc2) do
+  end
+end
+Calc.main()
+
