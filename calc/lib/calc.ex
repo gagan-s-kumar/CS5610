29bc26b3a6be88934e505989456ffb5efd487c73
gagan-s-kumar <gaganskumar9@gmail.com>
2018-02-04 10:33:39 -0500

    Basic computation working

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -34,10 +34,46 @@ defmodule Calc do
     push_to_stack((tl input), len-1, acc1, acc2, (hd input))
   end
 
+  def get_answer(op1, op2, operator) when operator == "+" do
+    op1 + op2
+  end
+
+  def get_answer(op1, op2, operator) when operator == "-" do
+    op1 - op2
+  end
+
+  def get_answer(op1, op2, operator) when operator == "*" do
+    op1 * op2
+  end
+
+  def get_answer(op1, op2, operator) do
+    op1 / op2
+  end
+
+  def do_compute(tuple) do
+    final_answer = tuple
+    IO.inspect(tuple)
+    val = elem(tuple, 0)
+    ops = elem(tuple, 1)
+    op1 = hd val
+    val = tl val
+    op2 = hd val
+    val = tl val
+    val = val ++ ["eos"]
+    operator = hd ops
+    ops = tl ops
+    answer = get_answer(op1, op2, operator)
+    IO.inspect(answer)
+    newval = [answer] ++ val
+    #newval = List.delete(newval, "eos")
+    {newval, ops}
+  end
+
   # Base case
   def push_to_stack(input, len, acc1, acc2, ch) when len == 0 do
-    {myInt, _} = Integer.parse(ch) 
-    IO.inspect({acc1++[myInt], acc2})
+    {myInt, _} = Integer.parse(ch) # Change this when () is added 
+    #IO.inspect({acc1++[myInt], acc2})
+    IO.inspect(do_compute({acc1++[myInt], acc2}))
   end
 
   def push_to_stack(input, len, acc1, acc2, ch) when ch == "+" do
