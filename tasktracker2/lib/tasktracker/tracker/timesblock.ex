863e9ff0dfbb650e89e5192784d627e8b26a1091
gagan-s-kumar <gaganskumar9@gmail.com>
2018-02-27 12:00:51 -0500

    Made changes to timesblock.ex Schema

--- tasktracker2/lib/tasktracker/tracker/timesblock.ex
+++ tasktracker2/lib/tasktracker/tracker/timesblock.ex
@@ -2,12 +2,13 @@ defmodule Tasktracker.Tracker.Timesblock do
   use Ecto.Schema
   import Ecto.Changeset
   alias Tasktracker.Tracker.Timesblock
+  alias Tasktracker.Tracker.Task
 
 
   schema "timesblocks" do
     field :end_time, :time
     field :start_time, :time
-    field :task_id, :id
+    belongs_to :task_id, Task
 
     timestamps()
   end
@@ -15,7 +16,7 @@ defmodule Tasktracker.Tracker.Timesblock do
   @doc false
   def changeset(%Timesblock{} = timesblock, attrs) do
     timesblock
-    |> cast(attrs, [:start_time, :end_time])
-    |> validate_required([:start_time, :end_time])
+    |> cast(attrs, [:start_time, :end_time, :task_id])
+    |> validate_required([:start_time, :end_time, :task_id])
   end
 end
