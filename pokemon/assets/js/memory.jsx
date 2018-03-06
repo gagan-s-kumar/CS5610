1b084a5f2949c372f46acdaffaa6fd8f7cf1dc64
gagan-s-kumar <gaganskumar9@gmail.com>
2018-03-06 10:35:03 -0500

    Pokemon first commit having memory as base

---
+++ pokemon/assets/js/memory.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'reactstrap';
+
+export default function game_init(root, channel) {
+  ReactDOM.render(<MemoryGame channel={channel} />, root);
+}
+
+class MemoryGame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.channel = props.channel;
+    this.state = { skel: "", goods: [], bads: [], max: 10, 
+                   cards: [],
+                   clicks: 0,
+                   score: 0,
+		   previous_card: "",
+		   active_cards: 0 };
+
+    this.channel.join()
+        .receive("ok", this.gotView.bind(this))
+        .receive("error", resp => { console.log("Unable to join", resp) });
+  }
+
+  gotView(view) {
+    console.log("New view", view);
+    this.setState(view.game);
+  }
+
+  change(pos) {
+    this.flip(pos)
+    this.flop(pos)
+  }
+
+  flip(pos) {
+    this.channel.push("flip", { pos: pos })
+        .receive("ok", this.gotView.bind(this));
+  }
+
+  flop(pos) {
+    this.channel.push("flop", { pos: pos })
+        .receive("ok", this.gotView.bind(this));
+  }
+
+  reset(trigger) {
+    this.channel.push("reset", { trigger: trigger })
+        .receive("ok", this.gotView.bind(this));
+  }
+
+  render() {
+    return (
+      <div className="container">
+ 		<div className="row">
+			<div className="col-md">
+				<Card root={this} pos={0} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={1} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={2} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={3} />	
+			</div>
+		</div>
+ 		<div className="row">
+			<div className="col-md">
+				<Card root={this} pos={4} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={5} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={6} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={7} />	
+			</div>
+		</div>
+ 		<div className="row">
+			<div className="col-md">
+				<Card root={this} pos={8} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={9} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={10} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={11} />	
+			</div>
+		</div>
+ 		<div className="row">
+			<div className="col-md">
+				<Card root={this} pos={12} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={13} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={14} />	
+			</div>
+			<div className="col-md">
+				<Card root={this} pos={15} />	
+			</div>
+		</div>
+		<button type="button" onClick={() => this.reset(0)}>Reset</button>
+		<h1>Score</h1>
+		<h1 id="score">{this.state.score}</h1>
+      </div>
+    );
+  }
+}
+
+function Card(params){
+  if(params.root.state.cards.length != 0) {
+	if(params.root.state.cards[params.pos].face == true && params.root.state.cards[params.pos].match == false){
+		let ele = params.root.state.cards[params.pos].value;
+		return	<div className="card" onClick={() => params.root.change(params.pos)}>
+			{ele}</div>
+	} else if(params.root.state.cards[params.pos].match == true){
+                let ele = params.root.state.cards[params.pos].value;
+                return  <div className="donecard">
+                        {ele}</div>
+	} else{
+		return	<div className="closecard" onClick={() => params.root.change(params.pos)}>
+		Click Me </div>;
+	}
+  } else {
+		return <div> Loading </div>
+  }
+}
+
